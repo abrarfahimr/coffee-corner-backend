8b4a48d06701dab485ee63671c99387214e83c5c
Abrar <abrarfahimr@gmail.com>
2022-09-27 21:33:38 -0400

    changed host to same as env variable

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 module.exports = {
   client: 'mysql',
   connection: {
-    host: '127.0.0.1',
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
